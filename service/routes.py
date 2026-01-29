d02618d5142880c990057e4f1a020b53085e6025
erikpablot28 <112137130+ErikTrivino@users.noreply.github.com>
2026-01-29 18:18:27 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LEER UNA CUENTA
 ######################################################################
@@ -64,12 +65,13 @@ def create_accounts():
 def get_accounts(account_id):
     """Lee una cuenta basada en el ID solicitado"""
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # LISTAR TODAS LAS CUENTAS
@@ -78,11 +80,12 @@ def get_accounts(account_id):
 def list_accounts():
     """Lista todas las cuentas disponibles"""
     app.logger.info("Request to list all Accounts")
-    
+
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    
+
     return jsonify(account_list), status.HTTP_200_OK
+
 
 ######################################################################
 # ACTUALIZAR UNA CUENTA EXISTENTE
@@ -91,15 +94,16 @@ def list_accounts():
 def update_accounts(account_id):
     """Actualiza una cuenta basada en el ID y el JSON enviado"""
     app.logger.info("Request to update Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
-    
+
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # ELIMINAR UNA CUENTA
@@ -108,15 +112,16 @@ def update_accounts(account_id):
 def delete_accounts(account_id):
     """Elimina una cuenta basada en el ID"""
     app.logger.info("Request to delete Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if account:
         account.delete()
-        
+
     return "", status.HTTP_204_NO_CONTENT
 
+
 ######################################################################
-#  U T I L I T Y   F U N C T I O N S
+#   U T I L I T Y   F U N C T I O N S
 ######################################################################
 
 
